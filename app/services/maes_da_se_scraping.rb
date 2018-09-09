f3a6a5d9da20688019b1f8fe48ee9cd82bcae54e
Jhonatan Martins <jhonatanbmartins@gmail.com>
2018-09-09 10:42:45 -0300

    fixes identation

--- app/services/maes_da_se_scraping.rb
+++ app/services/maes_da_se_scraping.rb
@@ -2,17 +2,17 @@ require "open-uri"
 require "nokogiri"
 
 class MaesDaSeScraping
-	def initialize(page=0)
-		@url = "http://www.desaparecidos.gov.br"
-		@page = page.to_s
-	end
+  def initialize(page=0)
+    @url = "http://www.desaparecidos.gov.br"
+    @page = page.to_s
+  end
 
-	def call
-		doc = parsed_template(@url + "/index.php/desparecidos?page=#{@page}")
-	end
+  def call
+    doc = parsed_template(@url + "/index.php/desparecidos?page=#{@page}")
+  end
 
-	private
-		def parsed_template(url)
-			return Nokogiri::HTML(open(url), nil, Encoding::UTF_8.to_s)
-		end
+  private
+    def parsed_template(url)
+      return Nokogiri::HTML(open(url), nil, Encoding::UTF_8.to_s)
+    end
 end
